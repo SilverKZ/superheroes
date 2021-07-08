a425639a6ca79cdb668fc6bad2ed7c55ef055b98
SilverKZ <silverkaz@mail.ru>
2021-07-09 01:21:32 +0600

    Tasks Complete

--- lib/widgets/superhero_card.dart
+++ lib/widgets/superhero_card.dart
@@ -6,53 +6,58 @@ class SuperheroCard extends StatelessWidget {
   final String name;
   final String realName;
   final String imageUrl;
+  final VoidCallback onTap;
 
   SuperheroCard({
     Key? key,
     required this.name,
     required this.realName,
     required this.imageUrl,
+    required this.onTap,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 70,
-      color: SuperheroesColors.backgroundCard,
-      child: Row(
-        children: [
-          Image.network(
-            imageUrl,
-            width: 70,
-            height: 70,
-            fit: BoxFit.cover,
-          ),
-          const SizedBox(width: 12),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  name.toUpperCase(),
-                  style: TextStyle(
-                    color: SuperheroesColors.textColor,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-                Text(
-                  realName,
-                  style: TextStyle(
-                    color: SuperheroesColors.textColor,
-                    fontSize: 14,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-              ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 70,
+        color: SuperheroesColors.backgroundCard,
+        child: Row(
+          children: [
+            Image.network(
+              imageUrl,
+              width: 70,
+              height: 70,
+              fit: BoxFit.cover,
             ),
-          ),
-        ],
+            const SizedBox(width: 12),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    name.toUpperCase(),
+                    style: TextStyle(
+                      color: SuperheroesColors.textColor,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  Text(
+                    realName,
+                    style: TextStyle(
+                      color: SuperheroesColors.textColor,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
